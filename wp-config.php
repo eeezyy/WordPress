6d1305dc26c366826cb6b0064faf87a5764c754c
Alexander Kumbeiz <alexander.kumbeiz@udm.at>
2014-09-30 11:16:55 +0200

    reading config from ENV

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'db');
+define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'LWWY4kIE8d9QzzvP');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', '172.17.42.1:49393');
+define('DB_HOST', getenv('DB_HOST') . ':' . getenv('DB_PORT'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
